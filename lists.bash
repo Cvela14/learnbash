72664b4b0b62922d2da7e3cca7f42f5643c4d728
Cvela14 <60629305+Cvela14@users.noreply.github.com>
2020-03-04 23:54:02 -0500

    Add files via upload

--- lists.bash
+++ lists.bash
@@ -1,11 +1,3 @@
-#  Using lists of commands (&, &&, || or ;) output First parameter is false.
-#   if the first positional parameter is false, the path to the current
-#   directory if the second parameter is true; if third parameter is true
-#   output the list of files in the current directory or Third parameter is
-#   false. if it is false.
-# echo ${*:1:1}
-$1 || echo First parameter is false.
+$1 || echo "First parameter is false."
 $2 && pwd
-$3 && ls 
-$3 || echo Third parameter is false.
-# RESULT=$((${*:1:1} * (${*:2:1} + (${*:3:1}))))
+$3 && ls || echo "Third parameter is false."
